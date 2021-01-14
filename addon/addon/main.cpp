7e582c0afda5a7dcaf1a08009fd410471b2344fd
daniel dan dan <cddanncppoli@gmail.com>
2021-01-14 14:02:33 -0400

    h

---
+++ addon/addon/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  addon
+//
+//  Created by daniel on 2021-01-14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
