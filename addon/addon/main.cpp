accefaa2446115d0d2f1f086a437f9deaf6ca5da
daniel dan dan <cddanncppoli@gmail.com>
2021-01-14 14:52:55 -0400

    commit to new change

--- addon/addon/main.cpp
+++ addon/addon/main.cpp
@@ -5,10 +5,36 @@
 //  Created by daniel on 2021-01-14.
 //
 
-#include <iostream>
+#include <node.h>
+#include <v8.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+static void Method(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  v8::Isolate* isolate = args.GetIsolate();
+  args.GetReturnValue().Set(v8::String::NewFromUtf8(
+        isolate, "world").ToLocalChecked());
 }
+
+// Not using the full NODE_MODULE_INIT() macro here because we want to test the
+// addon loader's reaction to the FakeInit() entry point below.
+extern "C" NODE_MODULE_EXPORT void
+NODE_MODULE_INITIALIZER(v8::Local<v8::Object> exports,
+                        v8::Local<v8::Value> module,
+                        v8::Local<v8::Context> context) {
+  NODE_SET_METHOD(exports, "hello", Method);
+}
+
+static void FakeInit(v8::Local<v8::Object> exports,
+                     v8::Local<v8::Value> module,
+                     v8::Local<v8::Context> context) {
+  auto isolate = context->GetIsolate();
+  auto exception = v8::Exception::Error(v8::String::NewFromUtf8(isolate,
+      "FakeInit should never run!").ToLocalChecked());
+  isolate->ThrowException(exception);
+}
+
+// Define a Node.js module, but with the wrong version. Node.js should still be
+// able to load this module, multiple times even, because it exposes the
+// specially named initializer above.
+#undef NODE_MODULE_VERSION
+#define NODE_MODULE_VERSION 3
+NODE_MODULE(NODE_GYP_MODULE_NAME, FakeInit)
